651bdca7f3a20f9636af5986e4b5e17f542e6db9
foshati <amirrezafoshati@gmail.com>
2025-08-06 12:39:42 +0330

    fix clerk and create information user page

--- src/app/components/layout/header.tsx
+++ src/app/components/layout/header.tsx
@@ -1,19 +1,17 @@
-import {  SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+'use client'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 
 export function Header() {
   return (
     <header style={{ display: 'flex', justifyContent: 'space-between', padding: 20 }}>
-      <Link href="/">ClerkNext</Link>
+      <Link className='font-bold' href="/">Next-clerk</Link>
       <SignedIn>
-        {/* Mount the UserButton component */}
         <UserButton />
       </SignedIn>
       <SignedOut>
-        {/* Signed out users get sign in button */}
         <SignInButton />
       </SignedOut>
     </header>
   )
 }
-
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased `}
         >
-          <Header/>
+          <Header />
           {children}
         </body>
       </html>
